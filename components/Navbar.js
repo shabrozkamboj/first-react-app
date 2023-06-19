92bc9189c78845165a5a6b7907f1391331ec6cbb
Shabroz Kamboj <98809822+shabrozkamboj@users.noreply.github.com>
2023-06-20 00:30:57 +0530

    Add files via upload

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+export default function Navbar(){
+return (
+    <nav className="nav1">
+        <img src={logo} className="logo" />
+        <h3>ReactFacts</h3>
+        <h4>React Course - Project 1</h4>
+    </nav>
+)
+}
